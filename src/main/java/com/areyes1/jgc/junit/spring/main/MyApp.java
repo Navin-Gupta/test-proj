e377a85d43c9f681b5ff53ab5f077bfaad0b618d
Navin <define.navin9@gmail.com>
2019-01-30 22:17:07 +0530

    First Commit

---
+++ src/main/java/com/areyes1/jgc/junit/spring/main/MyApp.java
@@ -0,0 +1,16 @@
+package com.areyes1.jgc.junit.spring.main;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.areyes1.jgc.junit.spring.service.SampleService;
+
+public class MyApp {
+
+	//@Autowired
+	//private SampleService service;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("HEllo All");
+	}
+
+}
